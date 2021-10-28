f74d36fe9a9c55b32d916686bfa3d9d1d8827945
OliviaPersson <olivia__p@hotmail.com>
2021-10-28 09:18:19 +0200

    Delete selected files method

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -157,6 +157,34 @@ namespace zipprogram
             }
         }
 
+        private void Delete_Button(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (object i in lbFiles.SelectedItems)
+            {
+                sb.Append(i.ToString());
+                sb.Append(" ");
+            }
+            string[] items = sb.ToString().Split(" ");
+
+            if (items[0] != "")
+            {
+                if (MessageBox.Show("Are you sure you want to delete the selected files?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        lbFiles.Items.Remove(items[i]);
+                    }
+                }
+                else {}
+            }
+            else
+            {
+                MessageBox.Show("Please Select a File to delete");
+            }
+            cbAllFeatures.IsChecked = false;
+        }
+
         private void MouseWheelScroll(object sender, MouseWheelEventArgs e)
         {
             ScrollViewer scv = (ScrollViewer)sender;
@@ -168,6 +196,11 @@ namespace zipprogram
         {
             var mainWindow = (MainWindow)Application.Current.MainWindow;
             mainWindow?.ChangeView(new MainPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 
